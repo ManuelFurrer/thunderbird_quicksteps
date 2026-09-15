ac7827209f7d7192b8c53805c7f8502121f031e3
Manuel <manuelfurrer13@gmail.com>
2026-09-15 21:40:03 +0200

    fix build-xpi.js script on windows

--- scripts/build-xpi.js
+++ scripts/build-xpi.js
@@ -1,48 +1,21 @@
 import fs from 'node:fs';
 import path from 'node:path';
-import { execFileSync } from 'node:child_process';
+import AdmZip from 'adm-zip';
 
 const rootDir = process.cwd();
 const srcDir = path.join(rootDir, 'src');
-const manifestPath = path.join(srcDir, 'manifest.json');
 const distDir = path.join(rootDir, 'dist');
+const manifest = JSON.parse(fs.readFileSync(path.join(srcDir, 'manifest.json'), 'utf8'));
 
-const manifest = JSON.parse(fs.readFileSync(manifestPath, 'utf8'));
-
-if (!manifest.version) {
-  throw new Error('Version not found in src/manifest.json');
+if (fs.existsSync(distDir)) {
+  fs.rmSync(distDir, { recursive: true, force: true });
 }
-
 fs.mkdirSync(distDir, { recursive: true });
 
-const zipFile = path.join(distDir, `quicksteps_${manifest.version}.zip`);
 const outputFile = path.join(distDir, `quicksteps_${manifest.version}.xpi`);
 
-if (fs.existsSync(zipFile)) {
-  fs.unlinkSync(zipFile);
-}
-
-if (fs.existsSync(outputFile)) {
-  fs.unlinkSync(outputFile);
-}
-
-if (process.platform === 'win32') {
-  execFileSync(
-    'powershell',
-    [
-      '-NoProfile',
-      '-Command',
-      `Compress-Archive -Path '${srcDir}\\*' -DestinationPath '${zipFile}'`
-    ],
-    { stdio: 'inherit' }
-  );
-
-  fs.renameSync(zipFile, outputFile);
-} else {
-  execFileSync('zip', ['-r', outputFile, '.'], {
-    cwd: srcDir,
-    stdio: 'inherit'
-  });
-}
+const zip = new AdmZip();
+zip.addLocalFolder(srcDir);
+zip.writeZip(outputFile);
 
 console.log(`Created ${path.relative(rootDir, outputFile)}`);
